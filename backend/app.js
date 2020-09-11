d408e1518a183a4a558943b8458731057b064673
Jorge <jorgediazok@gmail.com>
2020-09-11 00:22:20 -0300

    added more backend

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 require('./database');
+const userRoutes = require('./routes/user');
 
 //Middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //CORS
 app.use(cors());
 
 //Routes
-//app.use('/user', userRoutes);
+app.use('/user', userRoutes);
 
 //Errors handlers
 
